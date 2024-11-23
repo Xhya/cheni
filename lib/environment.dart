e1b7379bbb944db117ae6bcc6cff5768e258f30a
alexiasouvane <alexia.souvane@gmail.com>
2024-11-23 14:09:41 +0100

    add environment

--- lib/environment.dart
+++ lib/environment.dart
@@ -1,3 +1,11 @@
 const environment = {
   "API_URL": String.fromEnvironment('API_URL'),
+  "FIREBASE_MESSAGING_API_KEY":
+      String.fromEnvironment('FIREBASE_MESSAGING_API_KEY'),
+  "FIREBASE_MESSAGING_APP_ID":
+      String.fromEnvironment('FIREBASE_MESSAGING_APP_ID'),
+  "FIREBASE_MESSAGING_SENDER_ID":
+      String.fromEnvironment('FIREBASE_MESSAGING_SENDER_ID'),
+  "FIREBASE_MESSAGING_PROJECT_ID":
+      String.fromEnvironment('FIREBASE_MESSAGING_PROJECT_ID'),
 };
