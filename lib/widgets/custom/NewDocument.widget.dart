f699446f5870d12e65643887f3fe9375ea9d0e21
alexiasouvane <alexia.souvane@gmail.com>
2024-11-25 20:38:46 +0100

    bottom buttons

--- lib/widgets/custom/NewDocument.widget.dart
+++ lib/widgets/custom/NewDocument.widget.dart
@@ -1,7 +1,7 @@
 import 'package:cheni/actions/createDocument.action.dart';
-import 'package:cheni/enums/DocumentCategory.enum.dart';
 import 'package:cheni/utils/CheniColors.dart';
 import 'package:cheni/widgets/custom/DocumentCategoryList.widget.dart';
+import 'package:cheni/widgets/generic/CustomButton.widget.dart';
 import 'package:flutter/material.dart';
 
 class NewDocumentWidget extends StatefulWidget {
@@ -14,40 +14,62 @@ class NewDocumentWidget extends StatefulWidget {
 class _NewDocumentWidgetState extends State<NewDocumentWidget> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: CheniColors().background.one,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+    return Expanded(
+      flex: 1,
+      child: Stack(
         children: [
-          TextField(
-            onChanged: (String value) {
-              onUpdateDocumentName(value);
-            },
-            decoration: InputDecoration(
-              fillColor: Colors.white,
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(100),
-              ),
-              hintText: 'Nommer le document',
-              labelStyle: TextStyle(color: CheniColors().text.grey)
+          Container(
+            color: CheniColors().background.one,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextField(
+                  onChanged: (String value) {
+                    onUpdateDocumentName(value);
+                  },
+                  textCapitalization: TextCapitalization.sentences,
+                  decoration: InputDecoration(
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(100),
+                      ),
+                      hintText: 'Nommer le document',
+                      labelStyle: TextStyle(color: CheniColors().text.grey)),
+                  style: const TextStyle(fontSize: 12),
+                ),
+                const SizedBox(height: 16),
+                Text(
+                  "Catégories",
+                  style:
+                      TextStyle(color: CheniColors().text.black, fontSize: 10),
+                ),
+                const SizedBox(height: 4),
+                const DocumentCategoryList(displayChip: false),
+              ],
             ),
-            style: const TextStyle(fontSize: 12),
           ),
-          const SizedBox(height: 16),
-          Text(
-            "Catégories",
-            style: TextStyle(color: CheniColors().text.black, fontSize: 10),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                CustomButton(
+                  state: CustomButtonState(
+                    icon: Icons.arrow_back,
+                    onClick: () {},
+                  ),
+                ),
+                CustomButton(
+                  state: CustomButtonState(
+                    icon: Icons.check,
+                    onClick: () async {
+                      await onSubmitNewDocument();
+                    },
+                  ),
+                ),
+              ],
+            ),
           ),
-          const SizedBox(height: 4),
-          const DocumentCategoryList(displayChip: false, canClick: false),
-          // CustomButton(
-          //   state: CustomButtonState(
-          //     text: "Valider",
-          //     onClick: () async {
-          //       await onSubmitNewDocument();
-          //     },
-          //   ),
-          // )
         ],
       ),
     );
