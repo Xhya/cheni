fe66c336b28f1e36d3bece40857af2a843db4ec1
alexiasouvane <alexia.souvane@gmail.com>
2024-11-25 14:54:23 +0100

    DocumentCategoryTitle

--- lib/screens/DocumentCategory.screen.dart
+++ lib/screens/DocumentCategory.screen.dart
@@ -1,8 +1,6 @@
 import 'package:cheni/layout/Default.scaffold.dart';
-import 'package:cheni/services/Navigation.service.dart';
-import 'package:cheni/services/Translation.service.dart';
 import 'package:cheni/utils/CheniColors.dart';
-import 'package:cheni/utils/String.extension.dart';
+import 'package:cheni/widgets/custom/DocumentCategoryTitle.widget.dart';
 import 'package:cheni/widgets/generic/AsyncInitWidget.dart';
 import 'package:flutter/material.dart';
 
@@ -16,7 +14,6 @@ class DocumentCategory extends StatefulWidget {
 class _DocumentCategoryState extends State<DocumentCategory> {
   @override
   Widget build(BuildContext context) {
-    var navigationService = NavigationService();
 
     return DefaultScaffold(
       child: AsyncInitWidget(
@@ -28,25 +25,8 @@ class _DocumentCategoryState extends State<DocumentCategory> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Container(
-                width: double.maxFinite,
-                decoration: BoxDecoration(
-                  color: CheniColors().background.two,
-                  borderRadius: BorderRadius.circular(16),
-                ),
-                child: Container(
-                  padding: const EdgeInsets.only(top: 8, bottom: 8, left: 60),
-                  child: Text(
-                    t("document_category_enum_${navigationService.currentDocumentCategory?.label}")
-                        .toCapitalized,
-                    style: TextStyle(
-                      fontWeight: FontWeight.w700,
-                      color: CheniColors().text.secondary,
-                      fontSize: 20,
-                    ),
-                  ),
-                ),
-              ),
+              DocumentCategoryTitle(),
+              
             ],
           ),
         ),
---
+++ lib/widgets/custom/DocumentCategoryTitle.widget.dart
@@ -0,0 +1,38 @@
+import 'package:cheni/services/Navigation.service.dart';
+import 'package:cheni/services/Translation.service.dart';
+import 'package:cheni/utils/CheniColors.dart';
+import 'package:cheni/utils/String.extension.dart';
+import 'package:flutter/material.dart';
+
+class DocumentCategoryTitle extends StatefulWidget {
+  const DocumentCategoryTitle({super.key});
+
+  @override
+  State<DocumentCategoryTitle> createState() => _DocumentCategoryTitleState();
+}
+
+class _DocumentCategoryTitleState extends State<DocumentCategoryTitle> {
+  var navigationService = NavigationService();
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.maxFinite,
+      decoration: BoxDecoration(
+        color: CheniColors().background.two,
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Container(
+        padding: const EdgeInsets.only(top: 8, bottom: 8, left: 60),
+        child: Text(
+          t("document_category_enum_${navigationService.currentDocumentCategory?.label}")
+              .toCapitalized,
+          style: TextStyle(
+            fontWeight: FontWeight.w700,
+            color: CheniColors().text.secondary,
+            fontSize: 20,
+          ),
+        ),
+      ),
+    );
+  }
+}
