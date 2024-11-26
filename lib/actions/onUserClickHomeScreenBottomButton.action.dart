a7c87d38aca1613f656c82fe5733f594921b765c
alexiasouvane <alexia.souvane@gmail.com>
2024-11-26 13:54:56 +0100

    onUserClickHomeScreenBottomButton

---
+++ lib/actions/onUserClickHomeScreenBottomButton.action.dart
@@ -0,0 +1,58 @@
+import 'package:cheni/actions/onUserImportPDF.action.dart';
+import 'package:cheni/actions/onUserScanFile.action.dart';
+import 'package:cheni/services/Navigation.service.dart';
+import 'package:cheni/services/Translation.service.dart';
+import 'package:flutter/material.dart';
+
+onUserClickHomeScreenBottomButton() async {
+  // Open menu
+  navigationService.showMenu?.call(
+    items: [
+      PopupMenuItem(
+        onTap: () async {
+          await onUserImportPDF();
+        },
+        child: SizedBox(
+          width: double.maxFinite,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(t("import_file")),
+              const Icon(Icons.picture_as_pdf_outlined, size: 20),
+            ],
+          ),
+        ),
+      ),
+      PopupMenuItem(
+        onTap: () async {
+          // TODO: import pictures
+        },
+        child: SizedBox(
+          width: double.maxFinite,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(t("import_photo")),
+              const Icon(Icons.photo_camera_back_outlined, size: 20),
+            ],
+          ),
+        ),
+      ),
+      PopupMenuItem(
+        onTap: () async {
+          await onUserScanFile();
+        },
+        child: SizedBox(
+          width: double.maxFinite,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(t("scan_file")),
+              const Icon(Icons.camera_alt_outlined, size: 20),
+            ],
+          ),
+        ),
+      ),
+    ],
+  );
+}
